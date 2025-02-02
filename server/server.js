663ab43412124658588be5d930027b68f40b263a
bnour1 <brunowashingtong@gmail.com>
2025-02-02 13:08:03 -0300

    requests

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ exports = {
   // args is a JSON block containing the payload information.
   // args['iparam'] will contain the installation parameter values.
   onConversationCreate: function (args) {
-    let workspace = 7
-    if (args.iparams['Workspace ID'] == workspace) {
-      console.log("teste")
+    if(args.data.conversation.ticket_workspace_id == args.iparams['Workspace ID']){
+      console.log(args)
     }
+    
   },
   onAppInstallHandler: function (args) {
     console.info('onAppInstallHandler invoked with following data: \n', args);
@@ -15,5 +15,4 @@ exports = {
     console.log('onAppUninstalHandler invoked with following data: \n', args);
     renderData();
   }
-
 };
